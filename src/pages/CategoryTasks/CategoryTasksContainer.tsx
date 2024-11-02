e4d335f9ff4d375da69ebd8bc367da871a5151f7
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-02 09:13:13 +0330

    add add task button

--- src/pages/CategoryTasks/CategoryTasksContainer.tsx
+++ src/pages/CategoryTasks/CategoryTasksContainer.tsx
@@ -1,9 +1,13 @@
+import * as React from "react";
 import { useParams } from "react-router-dom"
 import { useTodoState } from "../../redux/hooks"
+import AddIcon from '@mui/icons-material/Add';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 export const CategoryTasks = () => {
   const { name } = useParams()
   const { todos } = useTodoState()
+  const [ isHovered, setIsHovered ] = React.useState(false)
 
   const getTodos = () => {
     if(!name || !todos)
@@ -11,9 +15,16 @@ export const CategoryTasks = () => {
     return todos[name].todos
   }
 
-  return <div className="w-full text-center">
-    {JSON.stringify(getTodos())}
+  return <div className="w-full text-start px-16">
+    <div className="text-2xl font-bold">
+      {name}
+    </div>
 
-    {`category ${name} tasks works!`}
+    <button onMouseEnter={() => setIsHovered(true)} onMouseLeave={() =>setIsHovered(false)} className="py-3 my-1">
+      <span className={`${isHovered ? 'text-text' : 'text-secondary'} transition-colors ml-2`}>
+        افزودن وظیفه
+      </span>
+      {isHovered ? <AddCircleIcon color="primary" /> : <AddIcon color="primary" /> }
+    </button>
   </div>
 }
